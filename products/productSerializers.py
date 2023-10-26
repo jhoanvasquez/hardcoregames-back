5d318c2e87c6d8c3ad9e85fffcd8a712d7675eb7
jmvasquezl <jmvasquezl@indracompany.com>
2023-10-26 11:24:57 -0500

    fix: ajuste serializador productos

--- products/productSerializers.py
+++ products/productSerializers.py
@@ -5,14 +5,12 @@ from products.models import Products
 class ProductsSerializer(serializers.ModelSerializer):
     class Meta:
         model = Products
-        fields = ('pk', 'title', 'description', 'stock', 'price', 'email_for_product',
-                  'pass_for_product', 'days_enable', 'date_register', 'image', 'date_last_modified',
+        fields = ('pk', 'title', 'description', 'stock', 'price', 'days_enable', 'date_register', 'image', 'date_last_modified',
                   'type_id', 'calification')
 
 
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Products
-        fields = ('pk', 'title', 'description', 'stock', 'price', 'email_for_product',
-                  'pass_for_product', 'days_enable', 'date_register', 'image', 'date_last_modified',
+        fields = ('pk', 'title', 'description', 'stock', 'price', 'days_enable', 'date_register', 'image', 'date_last_modified',
                   'type_id', 'calification')
