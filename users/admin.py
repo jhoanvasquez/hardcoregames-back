8f2b7f0b7418bdc51191c6f395113c33fa127941
jhoanvasquez <jhoan0498@gmail.com>
2026-04-01 17:58:07 -0500

    add page user sutomized up

--- users/admin.py
+++ users/admin.py
@@ -1,53 +1,25 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.models import User
-from django.urls import reverse
-from django.utils.html import format_html
 
 from users.models import User_Customized
 
 
-@admin.register(User_Customized)
-class UserCustomizedAdmin(admin.ModelAdmin):
+# ------------------------------------------------------------------ #
+#  Extend the built-in User admin with a link to User_Customized      #
+# ------------------------------------------------------------------ #
 
-    def get_queryset(self, request):
-        return super().get_queryset(request).select_related('user')
+class UserCustomizedInline(admin.StackedInline):
+    model = User_Customized
+    can_delete = False
+    verbose_name_plural = 'Perfil personalizado'
+    fields = ('phone_number', 'avatar', 'puntos')
 
-    # ---- column helpers ------------------------------------------------
 
-    @admin.display(description='Email / Usuario', ordering='user__username')
-    def col_username(self, obj):
-        url = reverse('admin:auth_user_change', args=[obj.user.id])
-        return format_html('<a href="{}">{}</a>', url, obj.user.username)
+class UserAdmin(BaseUserAdmin):
+    inlines = (UserCustomizedInline,)
+    list_display = ('username', 'email', 'first_name', 'last_name')
 
-    @admin.display(description='Nombre completo', ordering='user__first_name')
-    def col_full_name(self, obj):
-        return f'{obj.user.first_name} {obj.user.last_name}'.strip() or '—'
 
-    @admin.display(description='Activo', boolean=True, ordering='user__is_active')
-    def col_active(self, obj):
-        return obj.user.is_active
-
-    @admin.display(description='Puntos', ordering='puntos')
-    def col_puntos(self, obj):
-        return obj.puntos
-
-    list_display = (
-        'col_username',
-        'col_full_name',
-        'phone_number',
-        'col_puntos',
-        'col_active',
-    )
-    search_fields = ('user__username', 'user__first_name', 'user__last_name', 'phone_number')
-    ordering = ('user__username',)
-    list_per_page = 25
-
-    readonly_fields = ('col_username', 'col_full_name', 'col_active')
-    fieldsets = (
-        ('Datos del usuario', {
-            'fields': ('col_username', 'col_full_name', 'col_active'),
-        }),
-        ('Perfil', {
-            'fields': ('phone_number', 'avatar', 'puntos'),
-        }),
-    )
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
